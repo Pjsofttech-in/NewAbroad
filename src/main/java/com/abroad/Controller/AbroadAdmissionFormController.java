2319d6768575408eca62f0f6ba6351ab26d5e4bb
pooja patil <poojaspatil4322@gmail.com>
2026-01-12 14:37:26 +0530

    crossOrigin

--- src/main/java/com/abroad/Controller/AbroadAdmissionFormController.java
+++ src/main/java/com/abroad/Controller/AbroadAdmissionFormController.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "https://wayabroad.in")
 @RequestMapping("/admissionForms")
 public class AbroadAdmissionFormController {
 
--- src/main/java/com/abroad/Controller/AbroadRegisterController.java
+++ src/main/java/com/abroad/Controller/AbroadRegisterController.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
-
+@CrossOrigin(origins = "https://wayabroad.in")
 public class AbroadRegisterController {
 
     @Autowired

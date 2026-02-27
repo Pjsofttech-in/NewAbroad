6212925c6a7d320f69081855e158615f29676141
pooja patil <poojaspatil4322@gmail.com>
2026-02-27 17:35:36 +0530

    changes done gg

--- src/main/java/com/abroad/JWT/SecurityConfig.java
+++ src/main/java/com/abroad/JWT/SecurityConfig.java
@@ -48,7 +48,10 @@ public class SecurityConfig {
                     return config;
                 }))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/stafflogin","/userLogin","/permissionForUser","/getBranchCodeByUserEmail","/getAllBlogs","/getBlogByTitle","/createPartner","/createExamPreparation","/getExamById/{id}","/getAllExam","/getAllCourseName","/getAllStreams","/createRegisterForm","/createContactUS","/createAboutUs","/admissionForms/create","/getAllContinents","/hierarchy","/getAllCountries","/getAllStates","/getAllCities","/getAllUniversities","/getAllColleges","/getAllCourseName","/getAllStreams","/searchUniversities","/searchStates","/searchCities","/searchColleges","/searchCountries","/searchStreams").permitAll()
+                        .requestMatchers("/stafflogin","/userLogin","/permissionForUser","/getBranchCodeByUserEmail","/getAllBlogs","/getBlogByTitle","/createPartner",
+                                "/createExamPreparation","/getExamById/{id}","/getAllExam","/getAllCourseName","/getAllStreams","/createRegisterForm","/createContactUS","/createAboutUs",
+                                "/admissionForms/create","/getAllContinents","/hierarchy","/getAllCountries","/getAllStates","/getAllCities","/getAllUniversities","/getAllColleges","/getAllCourseName",
+                                "/getAllStreams","/searchUniversities","/searchStates","/searchCities","/searchColleges","/searchCountries","/searchStreams","/getBlogById/{id}").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
